4e8592ecf5a54369919b470170a70b587550d60b
matiasfarias152 <matiasfarias152@gmail.com>
2023-06-03 22:10:39 -0400

    dao y credencial

---
+++ credencial.py
@@ -0,0 +1,9 @@
+def get_credenciales():
+    credenciales = {
+        'host' : 'localhost',
+        'user' : 'root',
+        'password': '',
+        'database' : 'progra',
+        'port' : '3306'
+    }
+    return credenciales
