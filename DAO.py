c6d2895d86a3c3943e05b15d723738c94ff80ce1
electronightx <electronightx@gmail.com>
2023-06-07 18:59:51 -0400

    funcion crear usuario
    se creo una funcion que ingresa usuarios a la base de datos

--- DAO.py
+++ DAO.py
@@ -1,7 +1,7 @@
 #Aqui se trabajan las funciones que interactuen con el dao 
 import mysql.connector
 import credencial
-
+from User import User
 
 class DAO:
     def __init__(self):
@@ -15,6 +15,21 @@ class DAO:
     def cerrar(self):
         self.__conexion.commit()
         self.__conexion.close()        
+
+    def crear_usuario(self,usuario:User):
+        """
+        crear un nueva usuario en la base de datos.
+
+        recuperar el nombre de usuario y la contraseña ingresados y los inserta en la base de datos.
+        """
+
+        # Conectar en la base de datos.
+        self.conectar()
+        # crear el nombre de usuario y la contraseña en la base de datos.
+        sql = "INSERT INTO usuarios (username, password) VALUES (%s, %s)"
+        values = (self.get_username, self.get_password )
+        self.cursor.execute(sql, values)  
+        self.cerrar()
 
     def validar_credenciales(self) -> bool:
         """
