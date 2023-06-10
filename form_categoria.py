a23e543fe1aa0c885da88ad5b74c8d6036926d61
matiasfarias152 <matiasfarias152@gmail.com>
2023-06-09 22:52:56 -0400

    categoria comentario

--- form_categoria.py
+++ form_categoria.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter.ttk import Combobox
 
 
+
 formulario = tk.Tk()
 formulario.geometry("300x200") # Tamaño de la ventana
 formulario.title("Formulario de categorías") # Título de la ventana
@@ -9,6 +10,7 @@ formulario.title("Formulario de categorías") # Título de la ventana
 # Etiqueta
 categoria_etiqueta = tk.Label(formulario, text="Seleccione su categoría:") 
 categoria_etiqueta.pack() # La agregamos a la ventana
+#resultadocategorias = dao recupera categorias en tupla
 categorias = ["Ficción", "No ficción", "Fantasía", "Terror", "Biografía"]
 
 # Selector de categorías
@@ -17,6 +19,7 @@ categoria_seleccionar.pack() # Lo agregamos a la ventana
 
 def obtener_categoria():
     categoria = categoria_seleccionar.get()
+
     print("La categoría seleccionada es:", categoria)
 
 boton_obtener = tk.Button(formulario, text="Obtener categoría", command=obtener_categoria)
