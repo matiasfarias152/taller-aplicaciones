705a72ee5bac7f83784efab977311b5f80e67fee
Panxho-12 <alvarezfran125@gmail.com>
2023-06-14 12:07:20 -0400

    update

--- Formularios/form_categoria.py
+++ Formularios/form_categoria.py
@@ -10,29 +10,30 @@ for category in categories:
     categories_listbox.insert(tk.END, category)
     categories_listbox.pack(side=tk.LEFT)
 
+new_category_entry = tk.Entry(root)
+new_category_entry.pack(side=tk.TOP)
+
+def add_category():
+    new_category = new_category_entry.get()
+    if new_category:
+        categories_listbox.insert(tk.END, new_category)
+        new_category_entry.delete(0, tk.END)
+
 selected_categories_listbox = tk.Listbox(root)
 selected_categories_listbox.pack(side=tk.RIGHT)
 
 def move_category():
-# Obtener el índice de la categoría seleccionada
     selected_index = categories_listbox.curselection()
     if selected_index:
-# Obtener el valor de la categoría seleccionada
         selected_category = categories_listbox.get(selected_index[0])
-# Eliminar la categoría de la list box de categorías
         categories_listbox.delete(selected_index[0])
-# Agregar la categoría a la list box de categorías seleccionadas
         selected_categories_listbox.insert(tk.END, selected_category)
 
 def move_back_category():
-# Obtener el índice de la categoría seleccionada en la lista de categorías seleccionadas
     selected_index = selected_categories_listbox.curselection()
     if selected_index:
-# Obtener el valor de la categoría seleccionada
         selected_category = selected_categories_listbox.get(selected_index[0])
-# Eliminar la categoría de la lista de categorías seleccionadas
         selected_categories_listbox.delete(selected_index[0])
-# Agregar la categoría a la lista de categorías
         categories_listbox.insert(tk.END, selected_category)
 
 def return_values():
@@ -44,6 +45,9 @@ move_button.pack()
 
 move_back_button = tk.Button(root, text="Retirar", command=move_back_category)
 move_back_button.pack()
+
+add_category_button = tk.Button(root, text="Añadir categoría", command=add_category)
+add_category_button.pack()
 
 return_button = tk.Button(root, text="Retornar categorías seleccionadas", command=return_values)
 return_button.pack()
