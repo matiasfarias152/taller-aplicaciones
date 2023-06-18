abe3c60624ab871b2a489e7b2509b16000529988
Panxho-12 <alvarezfran125@gmail.com>
2023-06-17 23:18:20 -0400

    form_categoria

--- form_categoria.py
+++ form_categoria.py
@@ -6,27 +6,30 @@ categories_listbox = tk.Listbox(root)
 
 categories = ["Stephen King", "J.K. Rowling", "Dan Brown", "Agatha Christie", "Nicholas Sparks", "Malcolm X", "Michelle Obama", "Jamie Oliver", "Leonardo da Vinci", "Steve Jobs"]
 
-selected_categories_listbox = tk.Listbox(root)
-selected_categories_listbox.pack(side=tk.LEFT)
-selected_categories_listbox.configure(width=25, height=15)
+for category in categories:
+    categories_listbox.insert(tk.END, category)
+    categories_listbox.pack(side=tk.LEFT)
+    categories_listbox.configure(width=25, height=15)
+
+
 
 selected_categories_listbox = tk.Listbox(root)
 selected_categories_listbox.pack(side=tk.RIGHT)
 selected_categories_listbox.configure(width=25, height=15)
 
-def move_author():
+def move_categories():
     selected_index = categories_listbox.curselection()
     if selected_index:
-        selected_author = categories_listbox.get(selected_index[0])
+        selected_categories = categories_listbox.get(selected_index[0])
         categories_listbox.delete(selected_index[0])
-        selected_categories_listbox.insert(tk.END, selected_author)
+        selected_categories_listbox.insert(tk.END, selected_categories)
 
-def move_back_author():
+def move_back_categories():
     selected_index = selected_categories_listbox.curselection()
     if selected_index:
-        selected_author = selected_categories_listbox.get(selected_index[0])
+        selected_categories = selected_categories_listbox.get(selected_index[0])
         selected_categories_listbox.delete(selected_index[0])
-        categories_listbox.insert(tk.END, selected_author)
+        categories_listbox.insert(tk.END, selected_categories)
 
 def return_values():
     selected_categories = selected_categories_listbox.get(0, tk.END)
@@ -46,10 +49,10 @@ filter_entry.pack()
 filter_button = tk.Button(root, text="Buscar", command=filter_categories)
 filter_button.pack()
 
-move_button = tk.Button(root, text="Seleccionar", command=move_author)
+move_button = tk.Button(root, text="Seleccionar", command=move_categories)
 move_button.pack()
 
-move_back_button = tk.Button(root, text="Retirar", command=move_back_author)
+move_back_button = tk.Button(root, text="Retirar", command=move_back_categories)
 move_back_button.pack()
 
 return_button = tk.Button(root, text="Retornar autores seleccionados", command=return_values)
